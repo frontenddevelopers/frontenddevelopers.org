283b17d7b251e5a794f47cb1012ab78cea0e7b9c
Tony Phipps <tonyphipps@gmail.com>
2019-02-14 23:48:50 +0000

    feat: add ladning page

---
+++ src/components/LinkButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <a class="button" :href="link">
+    <slot/>
+  </a>
+</template>
+
+<script>
+export default {
+  name: "LinkButton",
+  props: {
+    link: {
+      String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.button {
+  @apply inline-flex items-center py-4 px-6 bg-blurple rounded-lg text-white font-semibold no-underline;
+}
+</style>
